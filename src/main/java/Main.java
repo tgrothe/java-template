f07d74a6f6871d236f0ecabf305a6a4800424782
Tobias Grothe <mail@tobias-1.de>
2025-11-11 12:25:11 +0100

    Default update at 2025-11-11-12-22 (#56)
    Improved logger configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-  public static final Logger logger = LogManager.getRootLogger();
+  public static final Logger logger = LogManager.getLogger();
 
   public static void main(String[] args) {
     Runtime.getRuntime()
