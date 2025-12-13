38e1eb723847d4b911cb8fec6de7a66520634156
Tobias Grothe <mail@tobias-1.de>
2025-12-13 10:10:36 +0100

    Update at 2025-12-13-10-09 (#66)
    Simplify Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,5 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Main {
-  public static final Logger logger = LogManager.getLogger();
+  // public static final Logger logger = LogManager.getLogger();
 
-  public static void main(String[] args) {
-    logger.info("Application started.");
-    // Your application logic here
-    logger.info("Application finished.");
-  }
+  public static void main(String[] args) {}
 }
