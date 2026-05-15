afbcf81e561fced6b52791e2182639015851c137
Tobias Grothe <tgrothe@users.noreply.github.com>
2026-05-15 18:51:53 +0200

    Update at 2026-05-15-18-51
    Logger adapted, tests commented out

---
+++ src/main/java/TempMain.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TempMain {
+  public static final Logger logger = LogManager.getLogger();
+
+  public static void main(String[] args) {
+    logger.info("Hello");
+  }
+}
